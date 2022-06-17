ce20206ffd9545f171c1f269209303a27b0e9244
yoge-adi <mailmeyogesh.more@gmail.com>
2022-06-17 11:56:33 +0530

    Second Commit

---
+++ src/test/java/DemoPackage1/Test12.java
@@ -0,0 +1,19 @@
+package DemoPackage1;
+
+public class Test12 {
+	public static void main(String[] args) {
+		
+		String hungrey = "Yes";
+		if(hungrey=="Yes") {
+			System.out.println("Eat something");
+		}
+		else
+		{
+			System.out.println("Do some work");
+		}
+		
+		
+		
+	}
+
+}
