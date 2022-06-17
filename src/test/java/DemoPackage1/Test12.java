d37dfa84a0323f3aa752c83780a1aa8578a69932
yogesh.more <yogesh.more@mechcl3pc37.PESMODERNCOE.EDU.IN>
2022-06-17 12:09:44 +0530

    Added Condition as per your instructions

--- src/test/java/DemoPackage1/Test12.java
+++ src/test/java/DemoPackage1/Test12.java
@@ -4,7 +4,7 @@ public class Test12 {
 	public static void main(String[] args) {
 		
 		String hungrey = "Yes";
-		if(hungrey=="Yes") {
+		if(hungrey=="Yes"||hungrey=="y"||hungrey=="yes"||hungrey=="Y") {
 			System.out.println("Eat something");
 		}
 		else
